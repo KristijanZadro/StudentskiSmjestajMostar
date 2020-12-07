587ed040f0e514e3d47d677485d3996170edbbb4
Kristijan Zadro <kristijan,zadroo30@gmail.com>
2020-12-07 16:36:10 +0100

    🗃database connect

--- server/server.js
+++ server/server.js
@@ -1,14 +1,29 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const cors = require("cors")
+const mysql = require("mysql")
 
 const app = express()
 
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "ReactMysql1233",
+    database: "ssmproject",
+    port: "3306"
+})
 
 app.use(bodyParser.json({extended: true}))
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(cors())
 
+db.connect(err => {
+    if(!err){
+        console.log("connected")
+    }else{
+        throw err
+    }
+})
 
 const PORT = process.env.PORT || 5000
 
