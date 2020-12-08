81541cc2887232732871caf30caed8e7b02491cb
lovrovidovic <lovrovidovic6@gmail.com>
2020-12-08 18:17:57 +0100

    delete trash

--- client/src/App.js
+++ client/src/App.js
@@ -2,19 +2,12 @@ import React from 'react';
 import './App.css';
 import {Switch, Route} from 'react-router-dom'
 
-import Register from './components/register/Register'
-import Login from './components/login/Login'
-import Home from './components/Home/Home'
 
 
 export default function App() {
   return (
     <div>
-      <Switch>
-        <Route path="/" component={Home} exact />
-        <Route path="/register" component={Register} exact />
-        <Route path="/login" component={Login} exact />
-      </Switch>
+      
     </div>
   )
 }
--- server/routes/register.js
+++ server/routes/register.js
@@ -2,3 +2,7 @@ const express = require("express")
 
 const router = express.Router()
 
+router.post('/api/register', (req,res) => {
+
+})
+
