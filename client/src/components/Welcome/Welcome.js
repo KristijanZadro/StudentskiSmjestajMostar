a2f62417ffabb7a6c6e87edcd50a6be04fa467f3
Kristijan Zadro <kristijan,zadroo30@gmail.com>
2020-12-10 18:24:50 +0100

    💄add welcome image

--- client/src/components/Welcome/Welcome.js
+++ client/src/components/Welcome/Welcome.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Content from '../../containers/Content/Content'
 import Header from '../../containers/Header/Header'
 
 export default function Welcome() {
     return (
         <div>
             <Header />
+            <Content />
         </div>
     )
 }
--- client/src/containers/Content/Content.js
+++ client/src/containers/Content/Content.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import './Content.css'
+import welcomePic from '../../images/details-4.jpeg'
 
 export default function Content() {
     return (
         <div className="content">
             <div className="img">
-
+                <img src={welcomePic} alt="" />
             </div>
             <div className="info">
 
