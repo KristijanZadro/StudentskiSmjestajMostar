69e4564a1cf330aac5e28a27cd07247def8494fc
Kristijan Zadro <kristijan,zadroo30@gmail.com>
2020-12-12 14:44:22 +0100

    ⚡️hashing password with bycript

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -3,25 +3,24 @@ const bcrypt = require("bcrypt")
 
 const registerController = (req,res,next) => {
 
-    const salt = bcrypt.genSalt(10)
-    const hashPassword = bcrypt.hash(password, salt)
-
     const name = req.body.name
     const surname = req.body.surname
     const email = req.body.email
     const password = req.body.password
     
-    
-
-    const SQL_INSERT = "INSERT INTO user (Ime, Prezime, Email, Password) VALUES (?,?,?,?);"
-    db.query(SQL_INSERT, [name, surname, email, hashPassword], (err, result) => {
-        if(err){
-            console.log(err)
-        }else{
-            console.log(result)
-            res.send(result)
-        }
+    bcrypt.hash(password, 10, (err, hash) => {
+        const SQL_INSERT = "INSERT INTO user (Ime, Prezime, Email, Password) VALUES (?,?,?,?);"
+        db.query(SQL_INSERT, [name, surname, email, hash], (err, result) => {
+            if(err){
+                console.log(err)
+            }else{
+                console.log(result)
+                res.send(result)
+            }
+        })
     })
+
+    
     
 }
 
