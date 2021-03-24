12239acfb7de5ad24908802f3682774da313be46
Kristijan Zadro <kristijan,zadroo30@gmail.com>
2021-03-24 12:49:01 +0100

    new build

--- client/src/index.js
+++ client/src/index.js
@@ -26,7 +26,7 @@ const store = createStore(
 
 ReactDOM.render(
     <Provider store={store}>
-      <Router basename="/">
+      <Router basename="">
         <App />
       </Router>
     </Provider>,
